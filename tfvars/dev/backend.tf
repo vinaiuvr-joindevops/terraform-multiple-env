240464c2b80db660a75467fdefdcc9a73072ef50
Vinai <uvr.vinaikumar@gmail.com>
2025-02-03 18:48:18 +0530

    Create backend.tf

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "devopsaws-82-remote-state-dev"
+key    = "expense-infra-dev" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+region = "us-east-1"
+dynamodb_table = "devopsaws-82-tf-remote-state-dev"
